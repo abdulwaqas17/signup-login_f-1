362a43414e67d22dbeef019d82b89c3d3e7a98f3
Abdul Waqas <www.waqas7172@gmail.com>
2025-02-27 22:42:04 +0500

    add env file

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,16 +4,18 @@ import {getAuth} from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB8NFuPkJmM5XHLerrMH5oQcpQOfyJZzfE",
-  authDomain: "myauthapp01.firebaseapp.com",
-  projectId: "myauthapp01",
-  storageBucket: "myauthapp01.firebasestorage.app",
-  messagingSenderId: "72802822633",
-  appId: "1:72802822633:web:e5218c620418ad37650378"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID, 
+  
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
